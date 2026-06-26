eb727e6a699014e5b07e7b88b6f67c3b027c15b3
Elecrow <24800915+Elecrow-RD@users.noreply.github.com>
2026-06-26 17:27:50 +0800

    --

--- example/V1.1_and_V1.2/ESP_IDF/Lesson03-LVGL_24_28/main/lv_ui/include/lv_ui.h
+++
@@ -1,8 +0,0 @@
-#ifndef LV_UI_H_
-#define LV_UI_H_
-
-#include "soft_drv_lvgl_port.h"
-
-void lv_ui_init(void);
-
-#endif
--- example/V1.1_and_V1.2/ESP_IDF/Lesson03-LVGL_24_28/main/soft_drv/include/soft_drv_lvgl_port.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOFT_DRV_LVGL_PORT_H_
-#define SOFT_DRV_LVGL_PORT_H_
-
-#include "esp_lvgl_port.h"
-
-void soft_drv_lvgl_port_init(void);
-
-#endif
